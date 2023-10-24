e06be14568ce5f309b31f6100dd1321cdc8e9b0d
Brian Ferry <bferry@redhat.com>
2023-10-24 14:37:43 -0400

    output panel

--- elements/hack-output/hack-output.ts
+++ elements/hack-output/hack-output.ts
@@ -5,6 +5,12 @@ import '@rhds/elements/rh-tabs/rh-tabs.js';
 import '@rhds/elements/rh-tabs/rh-tab.js';
 import '@rhds/elements/rh-tabs/rh-tab-panel.js';
 
+import '@rhds/elements/rh-accordion/rh-accordion.js';
+import '@rhds/elements/rh-accordion/rh-accordion-header.js';
+import '@rhds/elements/rh-accordion/rh-accordion-panel.js';
+
+import '@rhds/elements/rh-alert/rh-alert.js';
+
 import styles from './hack-output.css';
 
 /**
@@ -19,11 +25,80 @@ export class HackOutput extends LitElement {
     return html`
       <rh-tabs>
         <rh-tab slot="tab">Color Contrast</rh-tab>
-        <rh-tab-panel>Tab 1 content</rh-tab-panel>
+        <rh-tab-panel>
+          <h2>Results</h2>
+          <section>Your email has been checked and the tool found 3 color contrast errors.</section>
+
+          <rh-accordion>
+            <rh-accordion-header>Color contrast ratio is 4.03:1</rh-accordion-header>
+            <rh-accordion-panel>
+              <ul>
+                <li>WCAG AA: FAIL</li>
+                <li>WCAG AAA: FAIL</li>
+              </ul>
+                <rh-alert state="success">
+                  <h3 slot="header">Reccomendation</h3>
+                  <p>
+                    Looks like the <rh-cta> element is on a background image. Recommend editing the background image to be darker and checking the email again.
+                  </p>
+                </rh-alert>
+              </ul>
+            </rh-accordion-panel>
+            <rh-accordion-header>Color contrast ratio is 4.03:1</rh-accordion-header>
+            <rh-accordion-panel>
+              <ul>
+                <li>WCAG AA: FAIL</li>
+                <li>WCAG AAA: FAIL</li>
+
+                <rh-alert state="success">
+                  <h3 slot="header">Reccomendation</h3>
+                  <p>
+                    Looks like the <rh-cta> element is on a background image. Recommend editing the background image to be darker and checking the email again.
+                  </p>
+                </rh-alert>
+              </ul>
+            </rh-accordion-panel>
+            <rh-accordion-header>Color contrast ratio is 4.03:1</rh-accordion-header>
+            <rh-accordion-panel>
+              <ul>
+                <li>WCAG AA: FAIL</li>
+                <li>WCAG AAA: FAIL</li>
+
+                <rh-alert state="success">
+                  <h3 slot="header">Reccomendation</h3>
+                  <p>
+                    Looks like the <rh-cta> element is on a background image. Recommend editing the background image to be darker and checking the email again.
+                  </p>
+                </rh-alert>
+              </ul>
+            </rh-accordion-panel>
+          </rh-accordion>
+        </rh-tab-panel>
         <rh-tab slot="tab">Header</rh-tab>
-        <rh-tab-panel>Tab 2 content</rh-tab-panel>
+        <rh-tab-panel>
+          <h2>Results</h2>
+          <section>Your email has been checked and the tool found 0 header errors.</section>
+        </rh-tab-panel>
         <rh-tab slot="tab">Links</rh-tab>
-        <rh-tab-panel>Tab 3 content</rh-tab-panel>
+        <rh-tab-panel>
+          <h2>Results</h2>
+          <section>Your email has been checked and the tool found 4 link errors.</section>
+
+          <rh-accordion>
+            <rh-accordion-header>No underline</rh-accordion-header>
+            <rh-accordion-panel>
+              <p>Links require an underline to convey meaning in place of just using color.</p>
+
+              <rh-alert state="success">
+                <h3 slot="header">Reccomendation</h3>
+                <p>Add an underline to the link.</p>
+              </rh-alert>
+            </rh-accordion-panel>
+            <rh-accordion-header>Generic Link Text</rh-accordion-header>
+            <rh-accordion-header>Link text is too short</rh-accordion-header>
+            <rh-accordion-header>Generic Link Text</rh-accordion-header>
+          </rh-accordion>
+        </rh-tab-panel>
         <rh-tab slot="tab">Reccomendations</rh-tab>
         <rh-tab-panel>Tab 4 content</rh-tab-panel>
       </rh-tabs>
