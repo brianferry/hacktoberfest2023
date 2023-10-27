1de822b5c3ff4ded89c367f78b5c751631259636
Nikki Massaro Kauffman <nmassaro@redhat.com>
2023-10-27 14:43:02 -0400

    fix(app): updatyed resize CSS

--- elements/hack-app/hack-app.ts
+++ elements/hack-app/hack-app.ts
@@ -39,37 +39,39 @@ export class HackApp extends LitElement {
   render() {
     return html`
       <div class="container">
-        <div class="preview">
-          <hack-email>
-            <html>
-              <head>
-                <title>My Page</title>
-                <style>
-                  main {
-                    background-color: #bada55;
-                    height: 100%;
-                    display: flex;
-                    flex-flow: column;
-                  }
-                </style>
-              </head>
-              <body>
-                <header>
-                  <h1>Welcome to My Page</h1>
-                </header>
-                <main>
-                  <img src="https://via.placeholder.com/350x150" alt="placeholder">
-                  <p>This is the body of my page.</p>
-                </main>
-                <footer>
-                  <p>&copy; 2023 My Page</p>
-                </footer>
-              </body>
-            </html>
-          </hack-email>
-        </div>
-        <div class="editor">
-          <code-editor id="editor" language="html" read-only></code-editor>
+        <div class="input">
+          <div class="preview">
+            <hack-email>
+              <html>
+                <head>
+                  <title>My Page</title>
+                  <style>
+                    main {
+                      background-color: #bada55;
+                      height: 100%;
+                      display: flex;
+                      flex-flow: column;
+                    }
+                  </style>
+                </head>
+                <body>
+                  <header>
+                    <h1>Welcome to My Page</h1>
+                  </header>
+                  <main>
+                    <img src="https://via.placeholder.com/350x150" alt="placeholder">
+                    <p>This is the body of my page.</p>
+                  </main>
+                  <footer>
+                    <p>&copy; 2023 My Page</p>
+                  </footer>
+                </body>
+              </html>
+            </hack-email>
+          </div>
+          <div class="editor">
+            <code-editor id="editor" language="html" read-only></code-editor>
+          </div>
         </div>
         <div class="output">
           <hack-output></hack-output>
